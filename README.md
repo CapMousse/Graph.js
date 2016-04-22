db4b89dfd0149f9b78bf5f023830e223841b1aff
Jérémy Barbe <capitaine.mousse@gmail.com>
2016-04-23 00:01:30 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# graph
+# Graph.js
 
+![Graph](http://img.shwaark.com/uploads/big/14613584467762.png)
+
+Minimalistic line charts using canvas.
+
+## Installation
+Copy `src/graph.min.js` in your project and load it with a script tag :
+
+```html
+<script src="graph.min.js"></script>
+<script>
+    var chart = new Graph(canvas, datas, options)
+</script>
+```
+
+## Creating a chart
+To create a chart, you need to instantiate `Graph` with a canvas and a set of data.
+
+```javascript
+new Graph(HTMLCanvasElement, Array datas[, Object options]);
+```
+
+Bam, you're done !
+
+## Configuring a chart
+You can easily change some property of the chart with the config object :
+
+```javascript
+{
+    // Internal padding for the canvas
+    paddingBottom:  0, 
+    paddingLeft:    0,
+    paddingRight:   0,
+    paddingTop:     0,
+
+    // Canvas background
+    background:     "#FFFFFF",
+
+    // Show circle on the last data
+    showCircle:     true,
+    circle:         "#55AA77",
+    circleSize:     4,
+
+    // Show the line at zero
+    showZeroLine:   true,
+    zeroLineColor:  "#EEE",
+    lineColor:      "#8888EE",
+    lineWidth:      3,
+
+    // Show the bounds
+    showBounds:     true,
+    bounds:         "#8888EE",
+    boundsHeight:   14,
+    boundsFont:     "Arial"
+}
+```
+
+## License
+Graph.js is available under the [Mit License](http://opensource.org/licenses/MIT)
---
+++ src/graph.js
@@ -0,0 +1,171 @@
+(function(){
+    function Graph (data, canvas, options) {
+        this.options = {
+            paddingBottom:  0,
+            paddingLeft:    0,
+            paddingRight:   0,
+            paddingTop:     0,
+            showCircle:     false,
+            circle:         "#55AA77",
+            circleSize:     4,
+            background:     "#FFFFFF",
+            showZeroLine:   false,
+            zeroLineColor:  "#EEE",
+            lineColor:      "#8888EE",
+            lineWidth:      3,
+            showBounds:     false,
+            bounds:         "#8888EE",
+            boundsHeight:   14,
+            boundsFont:     "Arial"
+        };
+
+        if (!Array.isArray(data)) throw new Error('Data is not an array');
+        if (!canvas || !canvas.nodeName || canvas.nodeName !== "CANVAS") throw new Error('Canvas not defined');
+
+        this.data = data;
+        this.canvas = canvas;
+        this.context = canvas.getContext('2d');
+
+        if (options) {
+            for (var key in options) {
+                if (options.hasOwnProperty(key)) this.options[key] = options[key];
+            }
+        }
+
+        this.init();
+    }
+
+    /**
+     * Init graph
+     */
+    Graph.prototype.init = function () {
+        if (this.options.showBounds) {
+            this.options.paddingLeft = this.options.paddingLeft < 20 ? 20 : this.options.paddingLeft;
+            this.options.paddingTop = this.options.paddingTop < 20 ? 20 : this.options.paddingTop;
+            this.options.paddingBottom = this.options.paddingBottom < 20 ? 20 : this.options.paddingBottom;
+        }
+
+        this.drawBackground();
+        this.drawMiddle();
+        this.computeScale();
+        this.drawScale();
+        this.drawData();
+        this.drawCircle();
+        this.drawBounds();
+    }
+
+    /**
+     * Draw canvas brackground
+     */
+    Graph.prototype.drawBackground = function() {
+        this.context.fillStyle = this.options.background;
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    };
+
+    /**
+     * Draw middle line of a graph
+     */
+    Graph.prototype.drawMiddle = function() {
+        if (!this.options.showZeroLine) return;
+
+        var middle = Math.round(this.canvas.height / 2);
+
+        this.context.moveTo(this.options.paddingLeft, middle);
+        this.context.lineTo(this.canvas.width - this.options.paddingRight, middle);
+        this.context.strokeStyle = this.options.zeroLineColor;
+        this.context.stroke();
+    };
+
+    /**
+     * Compute scale for given canvas size
+     */
+    Graph.prototype.computeScale = function() {
+        var maxPositive = Math.abs(Math.max.apply(null, this.data)),
+            maxNegative = Math.abs(Math.min.apply(null, this.data)),
+            height      = this.canvas.height,
+            width       = this.canvas.width;
+
+
+        this.max = Math.max(maxPositive, maxNegative);
+
+        height -= this.options.paddingTop + this.options.paddingBottom;
+        width -= this.options.paddingLeft + this.options.paddingRight;
+
+        this.verticalScale = height / 2 / this.max;
+        this.horizontalScale = width / (this.data.length-1);
+    };
+
+    /**
+     * Draw scale line
+     */
+    Graph.prototype.drawScale = function () {
+        if (!this.options.showBounds) return;
+
+        this.context.moveTo(this.options.paddingLeft, this.options.paddingTop);
+        this.context.lineTo(this.options.paddingLeft, this.canvas.height - this.options.paddingBottom);
+        this.context.strokeStyle = this.options.zeroLineColor;
+        this.context.stroke();
+    };
+
+    /**
+     * Draw data line     
+     */
+    Graph.prototype.drawData = function() {
+        var i = this.data.length - 2;
+
+        this.context.strokeStyle = this.options.lineColor;
+        this.context.lineWidth = this.options.lineWidth;
+
+        this.context.beginPath();
+        this.context.moveTo.apply(this.context, this.getPointCoordinates(this.data.length - 1));
+
+        for (; i >= 0; i--) {
+            this.context.lineTo.apply(this.context, this.getPointCoordinates(i));
+        }
+
+        this.context.stroke();
+    };
+
+    /**
+     * Compute coordinates for asked data index
+     * @param  {index} index
+     * @return {array}
+     */
+    Graph.prototype.getPointCoordinates = function(index) {
+        return [
+            this.options.paddingLeft + (index * this.horizontalScale),
+            (this.canvas.height / 2) - (this.verticalScale * this.data[index])
+        ]
+    };
+
+    /**
+     * Draw circle to the end of the graph
+     */
+    Graph.prototype.drawCircle = function() {
+        if (!this.options.showCircle) return;
+
+        var lastPoint = this.getPointCoordinates(this.data.length - 1);
+
+        this.context.fillStyle = this.options.circle;
+        this.context.beginPath();
+        this.context.arc(lastPoint[0], lastPoint[1], this.options.circleSize, 0, 2*Math.PI);
+        this.context.closePath();
+        this.context.fill();
+    };
+
+    /**
+     * Draw scale bounds text
+     */
+    Graph.prototype.drawBounds = function() {
+        if (!this.options.showBounds) return;
+
+        this.context.font = this.options.boundsHeight + "px " + this.options.boundsFont;
+        this.context.fillStyle = this.options.bounds;
+        this.context.textBaseline = 'middle'; 
+        this.context.textAlign = 'center'; 
+        this.context.fillText(this.max, this.options.paddingLeft, this.options.paddingTop - this.options.boundsHeight);
+        this.context.fillText("-" + this.max, this.options.paddingLeft, this.canvas.height - this.options.paddingBottom + this.options.boundsHeight);
+    };
+
+    this.Graph = Graph;
+})(this);
